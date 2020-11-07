256bede8444febcf81b2c0e3ac4b740e7c306721
Ghidyon <gideonakunana@gmail.com>
2020-11-07 23:17:54 +0100

    added contact email validation

--- scripts/script.js
+++ scripts/script.js
@@ -55,22 +55,58 @@ const faqContent3 = document.querySelector('#faq-3');
 const faqContent4 = document.querySelector('#faq-4');
 
 faqHeader1.addEventListener("click", e => {
-    console.log(faqContent1);
+    faqHeader1.lastElementChild.classList.toggle('open');
+    faqHeader1.lastElementChild.firstElementChild.classList.toggle('red-stroke');
     faqContent1.classList.toggle('hide');
     faqContent1.classList.toggle('show');
 })
 
 faqHeader2.addEventListener("click", e => {
+    faqHeader2.lastElementChild.classList.toggle('open');
+    faqHeader2.lastElementChild.firstElementChild.classList.toggle('red-stroke');
     faqContent2.classList.toggle('hide');
     faqContent2.classList.toggle('show');
 })
 
 faqHeader3.addEventListener("click", e => {
+    faqHeader3.lastElementChild.classList.toggle('open');
+    faqHeader3.lastElementChild.firstElementChild.classList.toggle('red-stroke');
     faqContent3.classList.toggle('hide');
     faqContent3.classList.toggle('show');
 })
 
 faqHeader4.addEventListener("click", e => {
+    faqHeader4.lastElementChild.classList.toggle('open');
+    faqHeader4.lastElementChild.firstElementChild.classList.toggle('red-stroke');
     faqContent4.classList.toggle('hide');
     faqContent4.classList.toggle('show');
 })
+
+// Variables for Email Input 
+
+const button = document.querySelector('.contact-btn');
+
+const validateEmail = () => {
+    const matchEmail = /\S+@\S+.\S+/;
+    const input = document.querySelector('input');
+    const inputParent = document.querySelector('.email-field');
+    const error = document.querySelector('.email-field div');
+    const errorIcon = document.querySelector('.email-field img');
+
+    if (matchEmail.test(input.value)) {
+        error.classList.add('hide');
+        errorIcon.classList.add('hide');
+        inputParent.classList.remove('red-bg');
+        input.classList.remove('red-bd');
+    }
+    else {
+        error.classList.remove('hide');
+        errorIcon.classList.remove('hide');
+        error.textContent = "Whoops, make sure it's an email";
+        error.classList.add('red-bg', 'white');
+        inputParent.classList.add('red-bg');
+        input.classList.add('red-bd');
+    }
+}
+
+button.addEventListener("click", validateEmail);
